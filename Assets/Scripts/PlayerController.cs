3d47e2d9911fd6a00cec5dc3286e605067ce5495
ntd275 <ntd275@gmail.com>
2021-11-29 10:32:41 +0700

    add dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,11 +11,20 @@ public class PlayerController : MonoBehaviour
     public float JumpForce = 600;
     public GameObject BeforeJump;
     public GameObject AfterJump;
+    public float DashSpeed = 8000;
+    public float DashDuration = 0.5f;
+    public float DashCoolDown = 1;
+    public float AfterImageCoolDown = 0.1f;
 
     private float xMove = 0;
     private bool lookRight = true;
     private bool jump = false;
     private bool canJump = true;
+
+    private float dashTime = 0;
+    private float nextTimeCanDash = 0;
+    private float nextTimePrintAfterImage = 0;
+    private float now;
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
@@ -25,11 +34,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         xMove = Input.GetAxisRaw("Horizontal") * Speed;
-        animator.SetFloat("Speed", Mathf.Abs(xMove));
         if (Input.GetButtonDown("Jump") && canJump){
             jump = true;
         }
+        animator.SetFloat("Speed", Mathf.Abs(xMove));
         animator.SetFloat("VelocityY", rg.velocity.y);
+        now = Time.time;
+        if (Input.GetButtonDown("Dash") && now > nextTimeCanDash)
+        {
+            dashTime = now + DashDuration;
+            nextTimeCanDash = dashTime + DashCoolDown;
+            animator.SetTrigger("Dash");
+        }
+
+        if (now < dashTime && now > nextTimePrintAfterImage)
+        {
+            nextTimePrintAfterImage = now + AfterImageCoolDown;
+            GameObject trailPart = new GameObject();
+            SpriteRenderer trailPartRenderer = trailPart.AddComponent<SpriteRenderer>();
+            trailPartRenderer.sprite = GetComponent<SpriteRenderer>().sprite;
+            trailPart.transform.position = transform.position;
+            trailPart.transform.localScale = transform.localScale;
+            Destroy(trailPart, AfterImageCoolDown);
+        }
     }
 
     private void FixedUpdate()
@@ -39,12 +66,27 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
         rg.velocity = new Vector2(xMove * Time.fixedDeltaTime, rg.velocity.y);
+
         if(jump)
         {
             rg.AddForce(new Vector2(0, JumpForce));
             jump = false;
             canJump = false;
             Instantiate(BeforeJump, transform.position, Quaternion.identity);
+
+        }
+
+        now = Time.time;
+        if (now < dashTime)
+        {
+            if (lookRight)
+            {
+                rg.velocity = new Vector2(DashSpeed * Time.fixedDeltaTime, rg.velocity.y);
+            }
+            else
+            {
+                rg.velocity = new Vector2(-DashSpeed * Time.fixedDeltaTime, rg.velocity.y);
+            }
         }
     }
 
