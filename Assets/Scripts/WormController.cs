1fe56d2fa0968e86b614cbc40838d19fee09f9d4
NMHIEU2021999 <minhhieu20219992@gmail.com>
2021-12-04 15:53:27 +0700

    add worm enemy

---
+++ Assets/Scripts/WormController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public BoxCollider2D bc;
+    public Animator animator;
+    public float LimitX;
+    public float V;
+
+    private float walked = 0;
+    private bool isDead = false;
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            animator.SetTrigger("hit");
+            isDead = true;
+            V = 0;
+            Destroy(gameObject, 1f);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDead)
+        {
+            bc.enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(walked) >= LimitX)
+        {
+            Turn();
+        }
+        walked += isFacingRight ? V * Time.deltaTime : -V * Time.deltaTime;
+        rb.MovePosition(new Vector2(rb.position.x + (isFacingRight ? V * Time.deltaTime : -V * Time.deltaTime), rb.position.y));
+    }
+    private void Turn()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
