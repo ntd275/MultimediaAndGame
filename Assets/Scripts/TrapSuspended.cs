1e67f0b7daa6432ffa472e9501985ce9f9f10806
ntd275 <ntd275@gmail.com>
2021-12-13 18:22:21 +0700

    update

--- Assets/Scripts/TrapSuspended.cs
+++ Assets/Scripts/TrapSuspended.cs
@@ -6,19 +6,20 @@ public class TrapSuspended : MonoBehaviour
 {
     // Start is called before the first frame update
     public float LimitAngle = 60;
-    public Vector3 rotateVector = new Vector3(0, 0, 1);
+    public float Speed = 3;
+    private float rotated = 0;
+    private Vector3 rotateVector;
     void Start()
     {
-        
+        rotateVector = new Vector3(0, 0, Speed);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.Rotate(rotateVector);
-        var curAngle = transform.rotation.eulerAngles.z;
-        //Debug.Log(transform.rotation.eulerAngles.z);
-        if(curAngle >= LimitAngle && curAngle <= 360-LimitAngle)
+        rotated += rotateVector.z;
+        if(Mathf.Abs(rotated) >= LimitAngle)
         {
             rotateVector *= -1;
         }
